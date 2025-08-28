a88fbaf58c12e4724bdf17396e369c0c8484d4f5
Eishin <95329487+EishinCHEN@users.noreply.github.com>
2025-08-28 09:14:54 +0800

    dubug: Add html table length check

--- hsr_spider.py
+++ hsr_spider.py
@@ -12,13 +12,20 @@ def fetch_hsr_holiday_info() -> list[HolidayInfo]:
     holidays = []
     news = soup.find("div", class_ = "news")
 
+    comm_presale_date = ''
     # 需將bs4.element.ResultSet轉換為enumerate才可迭代
     for index, item in enumerate(news.find_all("tr")):
         # 跳過標題列 從第二筆開始擷取資料
         if index == 0:
             continue
+        
         tds = item.find_all("td")
-        presale_date = datetime.strptime(tds[2].text.split(' ')[0], "%Y/%m/%d").date()
+        if len(tds) >= 3:
+            presale_date = datetime.strptime(tds[2].text.split(' ')[0], "%Y/%m/%d").date()
+            if tds[2].has_attr('rowspan'):
+                comm_presale_date = presale_date
+        else :
+            presale_date = presale_date
         holidays.append(HolidayInfo("hsr", tds[0].text, tds[1].text, presale_date))
 
     return holidays
